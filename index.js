69c6cd0430cdfaa9a6a4f8a34eca5e312d8c8a1c
lucasjandrey <lucasjandrey5@gmail.com>
2022-12-23 16:15:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Get usuario
+//https://api.github.com/users/USERNAME
+
+//Get Repos
+//https://api.github.com/users/USERNAME/repos
+
+Alpine.data("index", () => {
+  return {
+    name: "lucasjandrey5",
+    getGitData(name) {
+      fetch("https://api.github.com/users/" + name)
+        .then((res) => {
+          return res.json();
+        })
+        .then((e) => {
+          this.current_user = e;
+        });
+    },
+    getGitRepos(name) {
+      fetch("https://api.github.com/users/" + name + "/repos")
+        .then((res) => {
+          return res.json();
+        })
+        .then((e) => {
+          this.current_repos = e;
+        });
+    },
+    current_user: null,
+    current_repos: null,
+    init() {
+      this.$watch("current_user", () => {
+        this.getGitRepos(this.name);
+      });
+    },
+  };
+});
+
+Alpine.start();
